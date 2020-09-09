8617038ed855f141d2b6e78c0a6613eda5b218ac
nolagx <arnon.h@campus.technion.ac.il>
2020-09-09 12:46:33 +0300

    query: moved "?" now parse the whole tree using early rule: removed "rule"

--- main.py
+++ main.py
@@ -28,22 +28,22 @@ def main():
     with open('grammar.lark', 'r') as grammar:
         # parser = Lark(grammar, parser='lalr', transformer=CalculateTree())
         # parser = Lark(grammar, parser='lalr', transformer=CalculateTree2())
-        parser = Lark(grammar, parser='earley', debug=False)
+        parser = Lark(grammar, parser='earley', debug=True)
 
-        test_input = 'a = "some string"\n'
+        test_input = '# a = "some" # "dwqdwq" \n'
         test_input += 'b = read("path")\n'
-        test_input += 'b = read(c)\n'
-        test_input += 'd = [1, 2)\n'
-        test_input += 'new parent(str, spn, str, str)\n'
-        test_input += 'cousin(x) <- parent("s", [1,2), x)\n'
-        test_input += 'cousin(x, y, z) <- extract RGX<"regex">(x, y, z) from a\n'
-        test_input += 'cousin(x, y, z) <- func<var>(x, y, z)\n'
-        test_input += 'cousin(x, y, z) <- func<var>(x, y, z), parent("s", [1,2), x), ' \
-                      'extract RGX<"regex">(x, y, z) from a\n'
-        test_input += 'parent("bob", "greg")\n'
-        test_input += 'parent(x, "greg", [10,13))\n'
-        test_input += '?parent("bob", "greg")\n'
-        test_input += '?parent(x, "greg", [10,13))\n'
+        # test_input += 'b = read(c)\n'
+        # test_input += 'd = [1, 2)\n'
+        # test_input += 'new parent(str, spn, str, str)\n'
+        # test_input += 'cousin(x) <- parent("s", [1,2), x)\n'
+        # test_input += 'cousin(x, y, z) <- extract RGX<"regex">(x, y, z) from a\n'
+        # test_input += 'cousin(x, y, z) <- func<var>(x, y, z)\n'
+        # test_input += 'cousin(x, y, z) <- func<var>(x, y, z), parent("s", [1,2), x), ' \
+        #               'extract RGX<"regex">(x, y, z) from a\n'
+        # test_input += 'parent("bob", "greg")\n'
+        # test_input += 'parent(x, "greg", [10,13))\n'
+        # test_input += '?parent("bob", "greg")\n'
+        # test_input += '?parent(x, "greg", [10,13))\n'
 
         # non_empty_lines = (line for line in test_input.splitlines() if len(line))
 
@@ -54,6 +54,7 @@ def main():
         parse_tree = parser.parse(test_input)
         parse_tree = TestTransformer().transform(parse_tree)
         print(parse_tree.pretty())
+        print(parse_tree)
 
 
 if __name__ == "__main__":
