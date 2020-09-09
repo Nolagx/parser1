d6de7799c1da7cc0039b6b9a0b14fda506589f86
nolagx <arnon.h@campus.technion.ac.il>
2020-09-09 15:46:14 +0300

    comment, assign var

--- main.py
+++ main.py
@@ -22,28 +22,30 @@ class CalculateTree(Transformer):
 class TestTransformer(Transformer):
     # get rid of "integer" in the tree
     integer = int
-
+    # comment = lambda self: "comment"
 
 def main():
     with open('grammar.lark', 'r') as grammar:
         # parser = Lark(grammar, parser='lalr', transformer=CalculateTree())
         # parser = Lark(grammar, parser='lalr', transformer=CalculateTree2())
-        parser = Lark(grammar, parser='earley', debug=True)
+        parser = Lark(grammar, parser='earley', debug=False)
 
-        test_input = '# a = "some" # "dwqdwq" \n'
+        test_input = 'a = "some" # "dwqdwq" \n'
+        test_input += 'a = b \n'
         test_input += 'b = read("path")\n'
-        # test_input += 'b = read(c)\n'
-        # test_input += 'd = [1, 2)\n'
-        # test_input += 'new parent(str, spn, str, str)\n'
-        # test_input += 'cousin(x) <- parent("s", [1,2), x)\n'
-        # test_input += 'cousin(x, y, z) <- extract RGX<"regex">(x, y, z) from a\n'
-        # test_input += 'cousin(x, y, z) <- func<var>(x, y, z)\n'
-        # test_input += 'cousin(x, y, z) <- func<var>(x, y, z), parent("s", [1,2), x), ' \
-        #               'extract RGX<"regex">(x, y, z) from a\n'
-        # test_input += 'parent("bob", "greg")\n'
-        # test_input += 'parent(x, "greg", [10,13))\n'
-        # test_input += '?parent("bob", "greg")\n'
-        # test_input += '?parent(x, "greg", [10,13))\n'
+        test_input += '#   b = #read("path")\n'
+        test_input += 'b = read(c)\n'
+        test_input += 'd = [1, 2)\n'
+        test_input += 'new parent(str, spn, str, str)\n'
+        test_input += 'cousin(x) <- parent("s", [1,2), x)\n'
+        test_input += 'cousin(x, y, z) <- extract RGX<"regex">(x, y, z) from a\n'
+        test_input += 'cousin(x, y, z) <- func<var>(x, y, z)\n'
+        test_input += 'cousin(x, y, z) <- func<var>(x, y, z), parent("s", [1,2), x), ' \
+                      'extract RGX<"regex">(x, y, z) from a\n'
+        test_input += 'parent("bob", "greg")\n'
+        test_input += 'parent(x, "greg", [10,13))\n'
+        test_input += '?parent("bob", "greg")\n'
+        test_input += '?parent(x, "greg", [10,13))\n'
 
         # non_empty_lines = (line for line in test_input.splitlines() if len(line))
 
