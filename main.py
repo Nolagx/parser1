1e5e7c65b6e18e5aa98671a6f377dbda7f1eb55f
nolagx <arnon.h@campus.technion.ac.il>
2020-09-14 23:07:33 +0300

    push for laptop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from lark import Lark, Transformer, v_args
+from lark import Lark, Transformer, v_args, Visitor
+from pyDatalog import pyDatalog
+
 
 @v_args(inline=True)
 class CalculateTree(Transformer):
@@ -23,6 +25,79 @@ class TestTransformer(Transformer):
     # get rid of "integer" in the tree
     integer = int
     # comment = lambda self: "comment"
+    # def multiline_string(self, *string_list):
+    #     result = ""
+    #     for string in string_list:
+    #         print(string)
+    #         result += string
+    #     return result
+    #
+    # def string(self, arg):
+    #     print("AAAAAAAAAAAAAAAAAAAAAA")
+
+
+@v_args(inline=False)
+class StringTransformer(Transformer):
+
+    def string(self, string_list):
+        result = ""
+        for token in string_list:
+            string = token[1:-1]
+            result += string
+        return result
+
+
+class Relation:
+    def __init__(self, name, terms):
+        self.name = name
+        self.terms = terms
+
+    def get_string_format(self):
+        ret = self.name + "("
+        for idx, term in enumerate(self.terms):
+            ret += term
+            if idx < len(self.terms) - 1:
+                ret += ", "
+        ret += ")"
+        return ret
+
+    def __repr__(self):
+        return self.get_string_format()
+
+@v_args(inline=False)
+class RelationTransformer(Transformer):
+
+    def relation(self, args):
+        name = args[0]
+        terms = args[1:]
+        return Relation(name, terms)
+
+
+class FactVisitor(Visitor):
+
+    def fact(self, tree):
+        assert tree.data == "fact"
+        relation = tree.children[0]
+        pyDatalog.assert_fact(relation.name, *relation.terms)
+
+
+class QueryVisitor(Visitor):
+
+    def query(self, tree):
+        assert tree.data == "query"
+        relation = tree.children[0]
+        print(pyDatalog.ask(relation.get_string_format()))
+
+
+@v_args(inline=False)
+class StringTransformer(Transformer):
+
+    def string(self, string_list):
+        result = ""
+        for token in string_list:
+            string = token[1:-1]
+            result += string
+        return result
 
 def main():
     with open('grammar.lark', 'r') as grammar:
@@ -30,34 +105,21 @@ def main():
         # parser = Lark(grammar, parser='lalr', transformer=CalculateTree2())
         parser = Lark(grammar, parser='earley', debug=False)
 
-        test_input = 'a = "some" # "dwqdwq" \n'
-        test_input += 'a = b \n'
-        test_input += 'b = read("path")\n'
-        test_input += '#   b = #read("path")\n'
-        test_input += 'b = read(c)\n'
-        test_input += 'd = [1, 2)\n'
-        test_input += 'new parent(str, spn, str, str)\n'
-        test_input += 'cousin(x) <- parent("s", [1,2), x)\n'
-        test_input += 'cousin(x, y, z) <- extract RGX<"regex">(x, y, z) from a\n'
-        test_input += 'cousin(x, y, z) <- func<var>(x, y, z)\n'
-        test_input += 'cousin(x, y, z) <- func<var>(x, y, z), parent("s", [1,2), x), ' \
-                      'extract RGX<"regex">(x, y, z) from a\n'
-        test_input += 'parent("bob", "greg")\n'
-        test_input += 'parent(x, "greg", [10,13))\n'
-        test_input += '?parent("bob", "greg")\n'
-        test_input += '?parent(x, "greg", [10,13))\n'
-        test_input += '?parent(x, "greg", [10,13))\n'
+        test_input = open("test_input").read()
+        parse_tree = parser.parse(test_input)
+        parse_tree = TestTransformer().transform(parse_tree)
+        parse_tree = StringTransformer().transform(parse_tree)
+        parse_tree = RelationTransformer().transform(parse_tree)
+        parse_tree = FactVisitor().visit(parse_tree)
+        parse_tree = QueryVisitor().visit(parse_tree)
+        print(parse_tree.pretty())
+        print(parse_tree)
 
         # non_empty_lines = (line for line in test_input.splitlines() if len(line))
 
         # for line in non_empty_lines:
         #     # print(line)
         #     print(parser.parse(line))
-
-        parse_tree = parser.parse(test_input)
-        parse_tree = TestTransformer().transform(parse_tree)
-        print(parse_tree.pretty())
-        print(parse_tree)
 
 
 if __name__ == "__main__":
