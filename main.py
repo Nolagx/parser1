4ed88e506ff81024ec44b197675c4a30140fb28c
Nolagx <33148180+Nolagx@users.noreply.github.com>
2020-09-18 12:46:05 +0300

    variable references checking (not complete)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from lark import Lark, Transformer, v_args, Visitor
+from lark.visitors import Visitor_Recursive
 from pyDatalog import pyDatalog
 
 
@@ -21,19 +22,27 @@ class CalculateTree(Transformer):
 
 
 @v_args(inline=True)
-class TestTransformer(Transformer):
+class CalculateTree(Transformer):
+    # noinspection PyUnresolvedReferences
+    from operator import add, sub, mul, truediv as div, neg
+    number = float
+
+    def __init__(self):
+        super().__init__()
+        self.vars = {}
+
+    def assign_var(self, name, value):
+        self.vars[name] = value
+        return value
+
+    def var(self, name):
+        return self.vars[name]
+
+
+@v_args(inline=True)
+class RemoveIntegerTransformer(Transformer):
     # get rid of "integer" in the tree
     integer = int
-    # comment = lambda self: "comment"
-    # def multiline_string(self, *string_list):
-    #     result = ""
-    #     for string in string_list:
-    #         print(string)
-    #         result += string
-    #     return result
-    #
-    # def string(self, arg):
-    #     print("AAAAAAAAAAAAAAAAAAAAAA")
 
 
 @v_args(inline=False)
@@ -64,6 +73,7 @@ class Relation:
     def __repr__(self):
         return self.get_string_format()
 
+
 @v_args(inline=False)
 class RelationTransformer(Transformer):
 
@@ -79,6 +89,81 @@ class FactVisitor(Visitor):
         assert tree.data == "fact"
         relation = tree.children[0]
         pyDatalog.assert_fact(relation.name, *relation.terms)
+
+
+class CheckReferencedVariablesVisitor(Visitor_Recursive):
+
+    def __init__(self):
+        super().__init__()
+        self.vars = set()
+
+    def assign_literal_string(self, tree):
+        assert_correct_node(tree, "assign_literal_string", 2, "name", "string")
+        var_name = tree.children[0].children[0]
+        self.vars.add(var_name)
+
+    def assign_string_from_file_string_param(self, tree):
+        assert_correct_node(tree, "assign_string_from_file_string_param", 2, "name", "string")
+        var_name = tree.children[0].children[0]
+        self.vars.add(var_name)
+
+    def assign_string_from_file_var_param(self, tree):
+        assert_correct_node(tree, "assign_string_from_file_var_param", 2, "name", "name")
+        right_var_name = tree.children[1].children[0]
+        if right_var_name not in self.vars:
+            raise NameError("variable " + right_var_name + " is not defined")
+        left_var_name = tree.children[0].children[0]
+        self.vars.add(left_var_name)
+
+    def assign_span(self, tree):
+        assert_correct_node(tree, "assign_span", 2, "name", "span")
+        var_name = tree.children[0].children[0]
+        self.vars.add(var_name)
+
+    def assign_var(self, tree):
+        assert_correct_node(tree, "assign_var", 2, "name", "name")
+        right_var_name = tree.children[1].children[0]
+        if right_var_name not in self.vars:
+            raise NameError("variable " + right_var_name + " is not defined")
+        left_var_name = tree.children[0].children[0]
+        self.vars.add(left_var_name)
+
+    # TODO after we decide what to do with free variables:
+    # TODO relation
+    # TODO rgx_relation
+    def rgx_relation(self, tree):
+        assert tree.data == "rgx_relation"
+        assert tree.children[0].data == "string"
+        assert tree.children[-1].data == "name"
+        var_name = tree.children[-1].children[0]
+        if var_name not in self.vars:
+            raise NameError("variable " + var_name + " is not defined")
+    # TODO ie relation
+    # TODO declare rules? (raise issue)
+
+    # TODO maybe use for variable assignment at a different visitor?
+    # def assign_var(self, name, value):
+    #     self.vars[name] = value
+    #     return value
+
+    # def var(self, name):
+    #     return self.vars[name]
+
+
+@v_args(inline=False)
+class RemoveTokensTransformer(Transformer):
+    def __init__(self):
+        super().__init__(visit_tokens=True)
+
+    def INT(self, args):
+        return args[0:]
+
+    def NAME(self, args):
+        return args[0:]
+
+    def STRING(self, args):
+        # removes the quotation marks
+        return args[1:-1]
 
 
 class QueryVisitor(Visitor):
@@ -99,6 +184,18 @@ class StringTransformer(Transformer):
             result += string
         return result
 
+
+def assert_correct_node(tree, node_name, len_children, *children_names):
+    assert tree.data == node_name, "bad node name: " + node_name + \
+                                   "\n actual node name: " + tree.data
+    assert len(tree.children) == len_children, "bad children length: " + str(len_children) + \
+                                               "\n actual children length: " + str(len(tree.children))
+    for idx, name in enumerate(children_names):
+        assert tree.children[idx].data == name, "bad child name at index " + str(idx) + ": " + \
+                                                name + \
+                                                "\n actual child name: " + tree.children[idx].data
+
+
 def main():
     with open('grammar.lark', 'r') as grammar:
         # parser = Lark(grammar, parser='lalr', transformer=CalculateTree())
@@ -107,11 +204,14 @@ def main():
 
         test_input = open("test_input").read()
         parse_tree = parser.parse(test_input)
-        parse_tree = TestTransformer().transform(parse_tree)
-        parse_tree = StringTransformer().transform(parse_tree)
-        parse_tree = RelationTransformer().transform(parse_tree)
-        parse_tree = FactVisitor().visit(parse_tree)
-        parse_tree = QueryVisitor().visit(parse_tree)
+        # parse_tree = StringTransformer().transform(parse_tree)
+        # parse_tree = RelationTransformer().transform(parse_tree)
+        # parse_tree = FactVisitor().visit(parse_tree)
+        # parse_tree = QueryVisitor().visit(parse_tree)
+        parse_tree = RemoveIntegerTransformer().transform(parse_tree)
+        parse_tree = RemoveTokensTransformer().transform(parse_tree)
+        CheckReferencedVariablesVisitor().visit(parse_tree)
+        print("===================")
         print(parse_tree.pretty())
         print(parse_tree)
 
