8445b6a177624ba158807d07041087722e26e31f
Nolagx <33148180+Nolagx@users.noreply.github.com>
2020-10-15 20:10:12 +0300

    reorganization of passes and run passes function

---
+++ backup.py
@@ -0,0 +1,64 @@
+from lark import Lark, Transformer, v_args, Visitor, Tree
+from lark.visitors import Interpreter, Visitor_Recursive
+import pyDatalog
+
+class Relation:
+    def __init__(self, name, terms):
+        self.name = name
+        self.terms = terms
+
+    def get_string_representation(self):
+        ret = self.name + "("
+        for idx, term in enumerate(self.terms):
+            ret += term
+            if idx < len(self.terms) - 1:
+                ret += ", "
+        ret += ")"
+        return ret
+
+    def __repr__(self):
+        return self.get_string_representation()
+
+
+class Span:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def get_string_representation(self):
+        return "[" + str(self.start) + ", " + str(self.stop) + ")"
+
+    def __repr__(self):
+        return self.get_string_representation()
+
+
+@v_args(inline=False)
+class RelationTransformer(Transformer):
+
+    def relation(self, args):
+        name = args[0]
+        terms = args[1:]
+        # return Relation(name, terms)
+        return Tree("relation", ["test"])
+
+
+class PyDatalogRepresentationVisitor(Visitor_Recursive):
+
+    def relation(self, tree):
+        pass
+
+
+class FactVisitor(Visitor):
+
+    def add_fact(self, tree):
+        assert tree.data == "add_fact"
+        relation = tree.children[0]
+        pyDatalog.assert_fact(relation.name, *relation.terms)
+
+
+class QueryVisitor(Visitor):
+
+    def query(self, tree):
+        assert tree.data == "query"
+        relation = tree.children[0]
+        print(pyDatalog.ask(relation.get_string_format()))
